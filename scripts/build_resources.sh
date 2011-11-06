840ccc98dcbf7cba2d2167a8e0822d565aa2f0de
Jared K. Smith <jsmith@fedoraproject.org>
2011-11-06 12:33:23 +0530

    Update builder script to fix a typo

--- scripts/build_resources.sh
+++ scripts/build_resources.sh
@@ -1,9 +1,10 @@
 for filename in `ls pot/*.pot`
 do
-	export short=`echo $filename | sed -e 's/.pot$//'`
+	short=`echo $filename | sed -e s/^pot.// | sed -e s/.pot$//`
+	long=`echo $filename | sed -e s/^pot.//`
 	echo [pym.$short]
 	echo file_filter = \<lang\>/$short.po
-	echo source_file = pot/$filename
+	echo source_file = pot/$long
 	echo source_lang = en
 
 done
