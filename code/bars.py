2a6505c613c4abc8f221176ff4738270e98c18f5
Kushal Das <kushal@fedoraproject.org>
2012-08-29 01:50:12 +0530

    Updated examples

--- code/bars.py
+++ code/bars.py
@@ -1,10 +1,34 @@
-#Bars module
+"""
+Bars Module
+============
+
+This is an example module with provide different ways to print bars.
+
+"""
 
 def starbar(num):
+    """
+    Prints a bar with *
+
+    :arg num: Length of the bar
+
+    """
     print '*' * num
 
 def hashbar(num):
+    """
+    Prints a bar with #
+
+    :arg num: Length of the bar
+
+    """
     print '#' * num
 
 def simplebar(num):
+    """
+    Prints a bar with -
+
+    :arg num: Length of the bar
+    
+    """
     print '-' * num
--- code/student_teacher.py
+++ code/student_teacher.py
@@ -1,37 +1,51 @@
 #!/usr/bin/env python
 
-class Person:
+class Person(object):
+    """
+    Returns a ```Person``` object with given name.
+
+    """
     def __init__(self,name):
         self.name = name
 
-    def getDetails(self):
+    def get_details(self):
+        "Returns a string containing name of the person"
         return self.name
 
 
 class Student(Person):
+    """
+    Returns a ```Student``` object, takes 3 arguments, name, branch, year.
+    
+    """
     def __init__(self,name,branch,year):
         Person.__init__(self,name)
         self.branch = branch
         self.year = year
 
-    def getDetails(self):
-        return (self.name, self.branch, self.year)
+    def get_details(self):
+        "Returns a string containing student's details."
+        return "%s studies %s and is in %s year." % (self.name, self.branch, self.year)
 
 
 class Teacher(Person):
-    def __init__(self,name,papers):
-        Person.__init__(self,name)
+    """
+    Returns a ```Teacher``` object, takes a list of strings (list of papers) as
+    argument.
+    """    
+    def __init__(self, name, papers):
+        Person.__init__(self, name)
         self.papers = papers
 
-    def getDetails(self):
-        return (self.name, self.papers)
+    def get_details(self):
+        return "%s teaches %s" % (self.name, ','.join(self.papers))
 
 
 person1 = Person('Rahul')
-student1 = Student('Kushal','CSE',2005)
-teacher1 = Teacher('Prashad',['C','C++'])
+student1 = Student('Kushal', 'CSE', 2005)
+teacher1 = Teacher('Prashad', ['C', 'C++'])
 
-print person1.getDetails()
-print student1.getDetails()
-print teacher1.getDetails()
+print person1.get_details()
+print student1.get_details()
+print teacher1.get_details()
 
