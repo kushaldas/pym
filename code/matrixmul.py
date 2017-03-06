f35b43a989c767391fa5b5ac205192b03a9864d0
Zafaryab <zaffuyab@gmail.com>
2017-03-06 20:14:19 +0530

    Missing built-in function sum() added, included loop, chaged index

--- code/matrixmul.py
+++ code/matrixmul.py
@@ -11,7 +11,7 @@ for i in range(0, n):
     b.append([int(x) for x in raw_input("").split(" ")])
 c = []
 for i in range(0, n):
-    c.append([a[i][j] * b[j][i] for j in range(0,n)])
+    c.append([sum([a[i][k] * b[k][j] for k in range(0,n)]) for j in range (0,n)])
 print "After matrix multiplication"
 print "-" * 10 * n
 for x in c:
